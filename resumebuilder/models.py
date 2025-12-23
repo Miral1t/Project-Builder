5160c146c40c8aae72f207291ee45aaceb9136c2
Miral1t <stankourij17@gmail.com>
2025-12-23 14:50:57 +0200

    template

--- resumebuilder/models.py
+++ resumebuilder/models.py
@@ -23,6 +23,8 @@ class Skill(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
